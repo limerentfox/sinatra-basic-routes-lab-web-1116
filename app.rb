19628f21a00f4dc933a8a5d84e6536479cfbaab5
limerentfox <christi@limerentfox.com>
2016-12-15 11:50:00 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Christi"
+  end
+
+  get '/hometown' do
+    "My hometown is Whitehouse Station"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Mumbai by Brika"
+  end
 end
